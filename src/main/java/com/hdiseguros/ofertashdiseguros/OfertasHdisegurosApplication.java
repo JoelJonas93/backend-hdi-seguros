8b54e41ccd3489aaad4a8f4c4392ceabc58a0017
DESKTOP-TK0J234\Unity <joel.oliveira@fatec.sp.gov.br>
2021-09-09 10:28:31 -0300

    first commit

---
+++ src/main/java/com/hdiseguros/ofertashdiseguros/OfertasHdisegurosApplication.java
@@ -0,0 +1,13 @@
+package com.hdiseguros.ofertashdiseguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfertasHdisegurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfertasHdisegurosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdiseguros/ofertashdiseguros/OfertasHdisegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdiseguros.ofertashdiseguros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfertasHdisegurosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
