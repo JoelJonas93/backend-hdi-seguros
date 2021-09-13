7bacd648ce949589a861bc724cd6805be7337367
DESKTOP-TK0J234\Unity <joel.oliveira@fatec.sp.gov.br>
2021-09-13 14:17:14 -0300

    rename method controller

--- src/main/java/com/hdiseguros/ofertashdiseguros/controller/CorretorController.java
+++ src/main/java/com/hdiseguros/ofertashdiseguros/controller/CorretorController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,14 @@ public class CorretorController {
     private CorretorService corretorService;
 
     @GetMapping("/{doc}")
-    public ResponseEntity<?> recoverWorkStation(@PathVariable("doc") String document) throws Exception {
+    public ResponseEntity<?> getCorretorActived(@PathVariable("doc") String document) throws Exception {
+        CorretorDTO corretorDTO = corretorService.validateCorretor(document);
+        return ResponseEntity.ok(corretorDTO);
+    }
+
+    @PutMapping("/{doc}/{isActive}")
+    public ResponseEntity<?> setActiveCorretor(@PathVariable("doc") String document,
+            @PathVariable("isActive") String isActive) throws Exception {
         CorretorDTO corretorDTO = corretorService.validateCorretor(document);
         return ResponseEntity.ok(corretorDTO);
     }
